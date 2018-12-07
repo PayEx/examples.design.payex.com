de6e3f1e2bb97c5f9176e95e0311591c5f322821
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-12-07 14:05:22 +0100

    Updated some validation stuff, barley started pw validation

--- signup/index.js
+++ signup/index.js
@@ -1,33 +1,33 @@
-const checkbox = document.querySelector("#privacy-policy");
+const checkbox = document.getElementById("privacy-policy");
 const btn = document.querySelector("#sign-up");
 const forms = document.querySelectorAll(".form-group")
-const validateForms = [...document.querySelectorAll(".form-group input")].filter(form => form.hasAttribute("required")).length;
-const pwVal = document.querySelector("#password");
-const confirmPwVal = document.querySelector("#confirm-password");
+const validateForms = [...document.querySelectorAll(".form-group input")].filter(form => form.hasAttribute("required"));
+const password = document.getElementById("password");
+const confirmpassword = document.getElementById("confirm-password");
 
 const toggleDisabled = () => {
     const validated = [...forms].filter(form => form.classList.contains("has-success"));
     const required = validated.filter(newlist => newlist.querySelector("input").hasAttribute("required"));
 
-    if (checkbox.checked && required.length >= validateForms) {
+    if (checkbox.checked && required.length >= validateForms.length) {
         btn.removeAttribute("disabled");
     } else {
         btn.setAttribute("disabled", "");
     }
 }
 
-if (forms.length > 0) {
-    forms.forEach(form => (
-        form.addEventListener("change", () => {
-            toggleDisabled();
-        })
-    ))
+if (forms.length && validateForms.length) {
+    if(checkbox){
+        checkbox.addEventListener("click", () => toggleDisabled());
+    }
+
+    validateForms.forEach(form => (
+        form.addEventListener("change", () => toggleDisabled())
+    ));
 }
 
-if (checkbox && validateForms > 0) {
-    checkbox.addEventListener("change", () => {
-        toggleDisabled();
+if(password && confirmpassword) {
+    password.addEventListener("change", () => {
+        
     });
 }
-
-// TODO: Fix password validation [AW]
